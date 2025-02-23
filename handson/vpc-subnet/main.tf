dad208b1e22ffc6e4ee28b648458f4227885a22f
牧野　弘明 <hiroaki@makinohiroakinoMacBook-Air.local>
2025-02-24 06:58:33 +0900

    first commit

---
+++ handson/vpc-subnet/main.tf
@@ -0,0 +1,21 @@
+# プロバイダーブロック
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+/*
+# VPCリソース
+resource "aws_vpc" "terra_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "aws_vpc_handson"
+  }
+}
+
+# サブネットリソース
+resource "aws_subnet" "terra_subnet" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  cidr_block = "10.0.0.0/24"
+}
+*/
---
+++ handson/web/locals.tf
@@ -0,0 +1,4 @@
+# locals {
+#   app_name    = "web"
+#   name_prefix = "${var.env}-${local.app_name}"
+# }
