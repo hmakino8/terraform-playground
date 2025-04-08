86b09b034f5ca53ae1504b254372ad0acf3215c7
hmakino8 <8makino.42tokyo@gmail.com>
2025-04-09 00:31:48 +0900

    change: optimizeCssを無効化

--- handson6/app/frontend/next.config.ts
+++ handson6/app/frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   optimizeFonts: false,
   experimental: {
-    optimizeCss: true,
+    // optimizeCss: true,
   },
   devIndicators: {
     appIsrStatus: false,
